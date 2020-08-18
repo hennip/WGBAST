de39e6b982ed2d3fb6c2a57791ba46da08c7458c
hennip <jeannie.jne@gmail.com>
2020-08-18 13:12:02 +0300

    paths updated

---
+++ 03-histmodel/paths_FLHM.R
@@ -0,0 +1,27 @@
+# cpouta ubuntu 18.04
+# PathFunctions<-"/home/ubuntu/WGBAST/00-functions/"
+# PathModel<-"/home/ubuntu/WGBAST/03-histmodel/"
+# PathData<-"/home/ubuntu/WGBAST/dat/"
+# PathOut<-"/home/ubuntu/WGBAST/out/"
+# Rpath<-'"/home/ubuntu/R/R-3.6.3/bin/x64/rterm.exe"'
+
+# laptop
+PathFunctions<-"C:/Rprojects/WGBAST/00-functions/"
+PathModel<-"C:/Rprojects/WGBAST/03-histmodel/"
+PathData<-"H:/Projects/WGBAST/FLHM/dat/"
+PathOut<-"C:/output/wgbast/flhm/"
+#Rpath<-'"C:/Users/03080932/Documents/R/R-3.6.3/bin/x64/rterm.exe"'
+
+# # TURD010
+# PathFunctions<-"C:/Rprojects/WGBAST/00-functions/"
+# PathModel<-"C:/Rprojects/WGBAST/03-histmodel/"
+# PathData<-"H:/Projects/WGBAST/FLHM/dat/"
+# PathOut<-"H:/Projects/WGBAST/FLHM/2020/out/"
+# Rpath<-'"C:/Program Files/R/R-3.6.2/bin/x64/rterm.exe"'
+
+# OULD017
+# PathFunctions<-"D:/R/WGBAST/00-functions/"
+# PathModel<-"D:/R/WGBAST/03-histmodel/"
+# PathData<-"H:/Projects/WGBAST/FLHM/2019/dat/"
+# PathOut<-"H:/Projects/WGBAST/FLHM/2019/out/"
+# Rpath<-'"C:/Program Files/R/R-3.6.2/bin/x64/rterm.exe"'
