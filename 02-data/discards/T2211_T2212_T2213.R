30975e1adb3db4b08bfe2664f68f81176cef9a13
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2025-03-28 12:21:54 +0200

    Report & discards tables added (wip)

---
+++ 02-data/discards/T2211_T2212_T2213.R
@@ -0,0 +1,54 @@
+source("02-data/discards/unrep-and-discards.R")
+
+# Table 2.2.1.2. Total catch: Nominal reported catches plus discards (incl. seal 
+# damaged salmon), unreported, and misreported catches of Baltic 
+# salmon in numbers from sea, coast, and river by country in 2014-2023 in Subdivisions 22-32
+
+# Countries 1=FI, 2=SE, 3=DK, 4=PL, 5=LV, 6=LT, 7=DE, 8=EE and 9=RU																			  						
+
+rep_catch<-LLD_N[,,1]+LLD_N[,,2]+
+River_N[,,1]+River_N[,,2]+
+Recr_N[,,1]+Recr_N[,,2]+
+GND_N[,,1]+GND_N[,,2]+
+FYK_N[,,1]+FYK_N[,,2]+
+MIS_N[,,1]+MIS_N[,,2]
+colnames(rep_catch)<-c("FI","SE","DK","PL","LV","LT","DE","EE","RU")
+
+tmp<-cbind(rep_catch[,3],rep_catch[,8],rep_catch[,1],rep_catch[,7],rep_catch[,5],rep_catch[,6],rep_catch[,4],rep_catch[,9],rep_catch[,2])
+
+tot<-c()
+for(i in 1:NumYears){
+  tot[i]<-sum(tmp[i,])
+}
+rep_catch<-cbind(tmp,tot)
+
+
+# Estimated misreported catch 
+misrep<-TMisr[,4,1]
+
+# Estimated unreported catch (median & 90%PI)
+A_TotUnrep_BS_res<-stats_y(A_TotUnrep_BS);unrep<-round(A_TotUnrep_BS_res,0)
+unrep<-as.data.frame(cbind(unrep[,2], str_c(unrep[,3], "-", unrep[,4])))
+
+
+# Estimated discarded catch
+A_TotDis_BS_res<-stats_y(A_TotDis_BS);dis<-round(A_TotDis_BS_res,0)
+dis<-as.data.frame(cbind(dis[,2], str_c(dis[,3], "-", dis[,4])))
+
+# Total catch
+A_TotCatch_BS_res<-stats_y(A_TotCatch_BS);totcatch<-round(A_TotCatch_BS_res,0)
+totcatch<-as.data.frame(cbind(totcatch[,2], str_c(totcatch[,3], "-", totcatch[,4])))
+
+T2212<-cbind(rep_catch,misrep, unrep, dis, totcatch)
+colnames(T2212)<-c("DE", "EE", "FI", "DE", "LV","LT","PL","RU","SE","Reported total",
+"Misreported","Unreported", "PI","Discarded","PI","Total","PI")
+write_xlsx(T2212, "../../WGBAST_shared/flhm/2025/dat/der/T2212.xlsx")
+
+
+
+
+
+
+
+
+
