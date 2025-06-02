d05dd4425ac4a718ef4789b8ca8fa07a7b47c3a0
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2025-06-02 12:48:54 +0300

    store trial versions. Add base4 JAGS model (assessment model) and it's Nimble version

---
+++ 03-flhm/Nimble/make_inits_Nimble.R
@@ -0,0 +1,325 @@
+
+
+# Becky:
+# ===============
+PathSim<-"C:/WGBAST15/Assessment results/" # results from the simulation model and output from scenarios
+
+
+#assessment_year<-2022
+#years<-length(seq(1987:assessment_year))
+#proj_years<-0
+#
+#maxage<-6
+#rstocks<-2 #Lule, Dal
+#AUS<-4
+#stocknames<-c("Torne", "Simo","Kalix","Rane","Pite","Aby","Byske","Rickle","Savaran",
+#              "Ume","Ore","Logde","Ljungan","Morrum","Eman","Kage","Test")
+
+
+AU<-c(1,1,1,1,2,2,2,2,2,2,2,2,3,4,4,2,3)
+e_delay<-c(rep(4,times=12),3,3,3,4,3)
+
+load(paste0(PathSim,"CR_2024_selected_chain.RData"))
+d<-as.matrix(chains)
+
+first.word <- function(my.string){
+    unlist(strsplit(my.string, "[",fixed=T))[1]
+}
+
+get.dims <- function(my.string){
+    tempstr<-unlist(strsplit(my.string, "[",fixed=T))[2]
+    matches <- regmatches(tempstr, gregexpr("[[:digit:]]+",tempstr))
+as.numeric(unlist(matches))
+}
+
+get_trunc_gamma<-function(shape,rate,xmin,xmax){
+x<-rgamma(1,shape=shape,rate=rate)
+while(x<xmin | x>xmax){
+x<-rgamma(1,shape=shape,rate=rate)
+}
+return(x)
+}
+
+get_q_init<-function(hr,E){
+return(log(-log(1-hr)/E))
+}
+ 
+
+make.inits<-function(){
+
+cnames<-colnames(d)
+vnames<-sapply(cnames,first.word)
+colnames(vnames)<-NULL
+vnames<-unique(vnames)
+
+#remove posterior predictive nodes
+pp<-grep("X",vnames)
+vnames<-vnames[-pp]
+
+vnames<-sort(vnames)
+
+inits_list<-vector("list", length = length(vnames))
+names(inits_list)<-vnames
+
+for(i in 1:length(vnames)){
+#dsub<-d[s,grep(paste0(vnames[i],"["),colnames(d),fixed=TRUE)]
+dsub<-d[,grep(vnames[i],colnames(d))]
+#find names containing "[" (arrays and vectors)
+if(length(grep(paste0(vnames[i],"["),colnames(dsub),fixed=TRUE))>0){
+dsub1<-d[,grep(paste0("^",vnames[i],"\\["),colnames(d))]
+nstring<-colnames(dsub1)[dim(dsub1)[2]]
+vdims<-get.dims(nstring) #get the dims
+inits_list[[i]]<-array(apply(dsub1,2,mean),dim=vdims)
+}else if(length(grep(paste0(vnames[i],"["),colnames(dsub),fixed=TRUE))==0){
+#else if(length(grep("[",nstring,fixed=T))==0){
+dsub1<-d[,grep(paste0("^",vnames[i],"$"),colnames(d))]
+names(dsub1)<-NULL
+inits_list[[i]]<-mean(dsub1)
+}
+}
+
+for(i in 1:length(inits_list)){
+tempdims<-dim(inits_list[[i]])
+#print(length(tempdims))          #max 3
+if(any(tempdims %in% c((years-2):(years+4)))){  #find year dims
+ydim<-which(tempdims %in% c((years-2):(years+4)))
+#print(paste(i," ", ydim))     #year is always the first dim...good
+   inits.add<-asub(inits_list[[i]],tempdims[tempdims %in% c((years-2):(years+4))],ydim)       #asub(x,index (of dim),dim)
+   inits_list[[i]]<-abind(inits_list[[i]],inits.add,inits.add,along=ydim)
+}
+
+}
+
+
+
+#add inits already earlier in WGBAST code
+#source(paste0("flhm/make_JAGS_data_",assessment_year,".r"))
+
+
+alpha_detect<-c(18,18,rep(10,times=15))
+beta_detect<-c(2,2,rep(10,times=15))
+
+
+inits.fn<-function() {
+list(early_MpsW=rlnorm(1,0.23,0.15),CV_MpsW=rbeta(1,30,70),Reff_mu=rbeta(1,15,35),Reff_eta=rbeta(1,10,10)*0.5,kE=array(rlnorm(years*6*6,0,0.0001),dim=c(years,6,6)),kEc=array(rlnorm(years*6,0,0.0001),dim=c(years,6)),kEdc=array(rlnorm(years*6,0,0.0001),dim=c(years,6)),mu_a=rnorm(2,-2.8,0.50),sd_a=rlnorm(2,log(0.20),0.20),a_slope=rnorm(17,-2.8,0.50),error_SR=array(rnorm(years*stocks,0,0.50),dim=c(years,stocks)),cv_SR=rlnorm(1,-1.5,0.20),Temp=rnorm(length(muTemp),muTemp,0.20),Parr=rbind(array(rlnorm(years*stocks,mu_Parr,sqrt(1/tau_Parr)),dim=c(years,stocks)),array(NA,dim=c(2,stocks)))) }
+
+inits.old<-inits.fn()
+initsall<-c(inits_list,inits.old)
+
+muLW_tmp<-array(0,dim=c(years+5,4))
+muLR_tmp<-array(0,dim=c(years+5,4))  
+for (i in 1:(years+5)){ # calendar years 1987-present+5
+	for (j in 1:4){ # sea ages (1=1SW, 2=2SW etc.)
+		muLW_tmp[i,j]<-initsall$cL[i]+initsall$bL[j]+initsall$delta[j]*initsall$Temp[i] # temperature effect differs on age groups
+    muLR_tmp[i,j]<-initsall$cL[i]+initsall$bL[j]+initsall$LReffect[j]+initsall$delta[j]*initsall$Temp[i]
+  }
+}
+   
+
+initsall$zz<-array(rnsbeta(years*6*6, 2, 2, min = 0.01, max = 0.04),dim=c(years,6,6))
+initsall$zzr<-array(rnsbeta(years*6, 2, 2, min = 0.01, max = 0.04),dim=c(years,6))
+initsall$zzc<-array(rnsbeta(years*6, 2, 2, min = 0.01, max = 0.04),dim=c(years,6))
+initsall$zzdc<-array(rnsbeta(years*6, 2, 2, min = 0.01, max = 0.04),dim=c(years,6))
+
+initsall$lw<-array(rnorm((years+5)*4,muLW_tmp,0.30),dim=c(years+5,4))  
+initsall$lr<-array(rnorm((years+5)*4,muLR_tmp,0.30),dim=c(years+5,4))
+
+initsall$RMps<-rbeta(years,initsall$Ra,initsall$Rb)
+initsall$cvDS<-rlnorm(1,-2.37,0.10)
+initsall$SCR<-rbeta(1,3,16)
+initsall$SCC<-rbeta(1,3,16)
+initsall$SCO<-rbeta(1,3,16)
+initsall$SCT<-rbeta(1,3,16)
+initsall$SCRW<-rbeta(1,3,16)
+
+initsall$tauqd<-get_trunc_gamma(10,1,1,10000)   #shape, rate, min, max
+initsall$tauql<-get_trunc_gamma(50,1,1,10000)
+initsall$tauqr<-get_trunc_gamma(10,1,1,10000)
+initsall$tauqcgn<-get_trunc_gamma(10,1,1,10000)
+initsall$tauqctn<-get_trunc_gamma(10,1,1,10000)
+
+initsall$rrW<-runif(6,50,150)
+initsall$rrRsp<-runif(6,50,150)
+initsall$rcW<-runif(6,50,150)
+initsall$rcR<-runif(6,50,150)
+initsall$rdW<-runif(6,50,150)
+initsall$rdR<-runif(6,50,150)
+initsall$rlW<-runif(6,50,150)
+initsall$rlR<-runif(6,50,150)
+
+initsall$corr_msw<-runif(stocks,0.10,0.40)
+
+initsall$mu_spawn<-rbeta(stocks,(mu_sp_alpha+1),(mu_sp_beta+1))
+initsall$CV_spawn<-rbeta(stocks,(CV_sp_alpha+1),(CV_sp_beta+1))
+
+initsall$muTrap<-c(rbeta(1,4,4),rbeta(1,150,400))
+initsall$etaTrap<-c(rlnorm(1,10,0.20),rlnorm(1,3.7,0.20))
+
+initsall$Usmolt<-runif(1,0.85,1.45) 		# Uncertainty factor
+
+
+#HARVEST RATES!
+#CATCHABILITIES?
+
+
+initsall$HRR<-c(rbeta(1,5,100),rbeta(5,5,25)) #,rbeta(5,25,5)
+initsall$qrW<-rlnorm(1,get_q_init(initsall$HRR[1],Er[1,1]),0.20)
+initsall$qrR<-c(rlnorm(1,get_q_init(initsall$HRR[1],Er[1,1]),0.20),rep(NA,times=5))
+
+initsall$HRL<-c(rbeta(1,5,100),rbeta(10,25,5))
+initsall$HRD<-c(rbeta(1,5,100),rbeta(10,25,5))
+
+initsall$qlW1<-rlnorm(1,get_q_init(initsall$HRL[1],El[1,1]),0.20)
+initsall$qlR1<-rlnorm(1,get_q_init(initsall$HRL[1],El[1,1]),0.20)
+
+initsall$qdW1<-rlnorm(1,get_q_init(initsall$HRD[1],Edo[1,1]),0.20)
+initsall$qdR1<-rlnorm(1,get_q_init(initsall$HRD[1],Edo[1,1]),0.20)
+
+initsall$HRCTN<-matrix(c(rbeta(3,5,100),rbeta(3,10,12.5),rbeta(12,10,25)),nrow=6,ncol=3,byrow=T)
+initsall$HRCGN<-matrix(c(rbeta(3,5,100),rbeta(3,10,12.5),rbeta(12,10,25)),nrow=6,ncol=3,byrow=T) 
+
+initsall$qctnW<-matrix(c(rlnorm(1,get_q_init(initsall$HRCTN[1,1],Ectn[1,1,1]),0.20),NA,NA,rlnorm(1,get_q_init(initsall$HRCTN[2,1],Ectn[1,1,1]),0.20),NA,NA,rlnorm(1,get_q_init(initsall$HRCTN[3,1],Ectn[1,1,1]),0.20),NA,NA,NA,NA,NA,NA,NA,NA,rlnorm(1,get_q_init(initsall$HRCTN[6,1],Ectn[1,1,1]),0.20),NA,NA),nrow=6,ncol=3,byrow=T)
+                    
+initsall$qctnR<-matrix(c(rlnorm(1,get_q_init(initsall$HRCTN[1,1],Ectn[1,1,1]),0.20),NA,NA,rlnorm(1,get_q_init(initsall$HRCTN[2,1],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[2,2],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[2,3],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[3,1],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[3,2],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[3,3],Ectn[1,1,1]),0.20),NA,NA,NA,NA,NA,NA,rlnorm(1,get_q_init(initsall$HRCTN[6,1],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[6,2],Ectn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCTN[6,3],Ectn[1,1,1]),0.20)),nrow=6,ncol=3,byrow=T)
+
+initsall$qcgnW<-matrix(c(rlnorm(1,get_q_init(initsall$HRCGN[1,1],Ecgn[1,1,1]),0.20),NA,NA,rlnorm(1,get_q_init(initsall$HRCGN[2,1],Ecgn[1,1,1]),0.20),NA,NA,rlnorm(1,get_q_init(initsall$HRCGN[3,1],Ecgn[1,1,1]),0.20),NA,NA,NA,NA,NA,NA,NA,NA,rlnorm(1,get_q_init(initsall$HRCGN[6,1],Ecgn[1,1,1]),0.20),NA,NA),nrow=6,ncol=3,byrow=T)
+                    
+initsall$qcgnR<-matrix(c(rlnorm(1,get_q_init(initsall$HRCGN[1,1],Ecgn[1,1,1]),0.20),NA,NA,rlnorm(1,get_q_init(initsall$HRCGN[2,1],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[2,2],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[2,3],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[3,1],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[3,2],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[3,3],Ecgn[1,1,1]),0.20),NA,NA,NA,NA,NA,NA,rlnorm(1,get_q_init(initsall$HRCGN[6,1],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[6,2],Ecgn[1,1,1]),0.20),rlnorm(1,get_q_init(initsall$HRCGN[6,3],Ecgn[1,1,1]),0.20)),nrow=6,ncol=3,byrow=T)
+
+initsall$sd_tr<-runif(1,0.05,1)
+initsall$phi_tr<-runif(1,0.1,0.9) 
+initsall$mean_trW<-rnorm(1,-5,0.2)  
+
+initsall$logitHtW2<-numeric(years+5)
+initsall$mu_trW<-numeric(years+5)
+
+initsall$logitHtW2[1]<-rnorm(1,-5,0.2) 
+initsall$mu_trW[1]<-1
+
+for(i in 1:(years+4)){ # i: calendar year
+
+  initsall$mu_trW[i+1]<-initsall$phi_tr*initsall$logitHtW2[i]+(1-initsall$phi_tr)*initsall$mean_trW
+  initsall$logitHtW2[i+1]<-rnorm(1,initsall$mu_trW[i+1],0.01)
+}
+
+#########################
+
+initsall$sd_ql<-runif(1,0.05,1)
+initsall$phi_ql<-runif(1,0.1,0.9) 
+initsall$mean_qlW<-rnorm(1,-5,0.2) 
+
+initsall$logit_qlW<-numeric(years+4)
+initsall$mu_qlW<-numeric(years+4)
+
+initsall$logit_qlW[1]<-rnorm(1,-5,0.2) 
+initsall$mu_qlW[1]<-1 #not used anywhere
+
+for(i in 1:(years+3)){ # i: calendar year
+
+  initsall$mu_qlW[i+1]<-initsall$phi_ql*initsall$logit_qlW[i]+(1-initsall$phi_ql)*initsall$mean_qlW
+  initsall$logit_qlW[i+1]<-rnorm(1,initsall$mu_qlW[i+1],0.01)
+}
+
+#############################
+
+initsall$sd_qd<-runif(1,0.05,1)
+initsall$phi_qd<-runif(1,0.1,0.9) 
+initsall$eff_qd<-rbeta(1,20,10)
+
+initsall$mean_qdW<-numeric(3)
+
+initsall$mean_qdW[1]<-NA
+initsall$mean_qdW[2]<-rnorm(1,-5,0.2)  # 2SW
+initsall$mean_qdW[3]<-initsall$mean_qdW[2]*initsall$eff_qd
+
+#*************************** 
+
+initsall$logit_qdW<-array(NA,dim=c(years+5,maxage))  
+initsall$mu_qdW<-array(NA,dim=c(years+5,maxage))
+
+for(j in 2:3){
+
+ # implies uniform[0,1] prior for initial catchability
+  initsall$logit_qdW[1,j]<-rnorm(1,-5,0.2) 
+  initsall$mu_qdW[1,j]<-1 
+  
+  for(i in 1:(years+4)){
+    # mean reverting AR(1) (index i is year)
+    initsall$mu_qdW[i+1,j]<-initsall$phi_qd*initsall$logit_qdW[i,j]+(1-initsall$phi_qd)*initsall$mean_qdW[j]
+    initsall$logit_qdW[i+1,j]<-rnorm(1,initsall$mu_qdW[i+1,j], 0.01)
+
+    }
+}
+
+initsall$HtW<-rbind(initsall$HtW,initsall$HtW[(years-3):years,])
+initsall$HtR<-rbind(initsall$HtR,initsall$HtR[(years-3):years,]) 
+
+initsall$qdW<-NULL
+initsall$qdR<-NULL
+initsall$qlW<-NULL
+initsall$qlR<-NULL
+
+initsall$eLW<-rnorm(years+5,0,0.5)
+initsall$eLR<-rnorm(years+5,0,0.5)
+
+initsall$SmoltW[1:10,]<-initsall$SmoltW[1:10,]*2
+#initsall$K<-initsall$K*10
+
+initsall$CV_HrW<-runif(1,0.01,1)             
+initsall$logit_deltaHRW<-rnorm(1,0.85,0.25)
+initsall$mu_HrW<-rnorm(stocks,-1.3862944 ,0.3)
+initsall$HrW_autoc<-runif(stocks,0.1,0.99)
+
+initsall$HrW<-NULL
+
+initsall$logit_HrW<-array(NA,dim=c((years+3),6,stocks))
+initsall$logit_HrW[,3,]<-rnorm((years+3)*stocks,-1.4,0.50)
+initsall$coefDS_tmp<-rlnorm(1,-2.029014,0.1211)
+ 
+initsall$CV_ladder<-rlnorm(1,-3,0.20)
+initsall$mu_CVSR<-rnorm(1,-1.5,0.20)
+initsall$tau_CVSR<-runif(1,5,30)
+initsall$cv_SR<-rlnorm(stocks,-1.5,0.50)
+#initsall$cv_SR<-rlnorm(1,-1.5,0.50) 
+
+initsall$logit_mu_spawn<-rnorm(stocks,mu_mu_sp,sqrt(1/(tau_mu_sp*5)))
+initsall$logit_CV_spawn<-rnorm(stocks,mu_CV_sp,sqrt(1/(tau_CV_sp*5)))
+
+initsall$logit_mu_spawn[1]<-runif(1,0.85,3) #lower bound 0.70
+initsall$ logit_CV_spawn[1]<-runif(1,-4,-2.5) 
+
+initsall$logit_mu_spawn[3]<-runif(1,0,3) #lower bound 0.70
+initsall$ logit_CV_spawn[3]<-runif(1,-3,-0.4) 
+
+initsall$logit_pdetect<-matrix(rnorm((years+5)*stocks,initsall$logit_mu_spawn,rep(0.20,times=stocks)),nrow=(years+5),byrow=T)   
+    
+initsall$M74<-NULL
+initsall$M74<-array(NA,dim=c((years),stocks))
+for(s in 1:stocks){
+initsall$M74[,s]<-rbeta(years,M74_alpha[,s]*2,M74_beta[,s]*2)
+}
+
+initsall$logit_M74<-matrix(rnorm(years*stocks,M74_mu,sqrt(1/M74_tau)),nrow=years)
+return(initsall)
+ 
+}
+
+#
+# # parameters of the gamma
+# x <- seq(0,1,by=0.001)
+#  jagsgamma <- function(x, r, mu) {(mu^r*x^(r-1)*exp(-mu*x))/gamma(r)}
+# jagsr=2
+# jagsmu=50
+# # plot the density using the formula of jags
+#    plot(x, jagsgamma(x, jagsr, jagsmu), type="l", xlab="x", ylab="Density")
+#    #plot the density using the dgamma of r
+#    par(new=TRUE)
+#     plot(x, dgamma(x, shape=jagsr, rate=jagsmu), type="l", axes=FALSE, 
+#     col="red", xlab="", ylab="")
+#
+#
+#gvars<-rgamma(1000,shape=jagsr,rate=jagsmu)
+#gvars<-gvars[gvars<0.5/12]
+#
+#bvars<-rnsbeta(1000, 2, 2, min = 0, max = 0.5/12)
+
---
+++ 03-flhm/Nimble/run_FLHM_2025_Nimble_base4.R
@@ -0,0 +1,184 @@
+
+#river migration survival by stock/AU - currently wild stocks only (check) 
+#Only Ume/Vindel currently has reduced spawner survival in some years
+#Note that if extra mortality is added for wild stocks 1-4 in years with tagging data, 
+#TxWs need to move into a loop where s is stock index, not AU - do something about this - use average surv_migr over stocks in each AU?
+#In this version river is survrR or survrW and surv are re-numbered
+#Now HrW instead of HrR for reared spawners (Torne and Simo) in population dynamics (tagged and untagged)
+#Parr added to NrRsp not NrW (wild)
+
+#flexible indexing not complete!!
+library(abind)
+library(coda)
+library(nimble)
+library(nimbleHMC)
+library(parallel)
+library(reshape2)
+library(extraDistr)
+
+parallel<-F
+CR<-T
+
+
+assessment_year<-2025
+years<-length(seq(1987:assessment_year))
+proj_years<-0
+maxage<-6
+rstocks<-2 #Lule, Dal
+stock_indices<-c(1:17) #17
+#NB if run with Torne and Simo they should have positions 1 and 2 because of exceptions for these rivers
+stocks<-length(stock_indices)
+allstocks<-17 #number of stocks in data files
+AUS<-4
+trolling<-1
+
+##POPULATION DYNAMICS
+
+#stock codes
+#1 "Tornionjoki"
+#2 "Simojoki"
+#3 "Kalixalven"
+#4 "Ranealven"
+#5 "Pitealven"
+#6 "Abyalven"
+#7 "Byskealven"
+#8 "Ricklean"
+#9 "Savaran"
+#10 "Vindelalven"
+#11 "Orealven"
+#12 "Logdealven"
+#13 "Ljungan"
+#14 "Morrumsan"
+#15 "Eman"
+#16 "Kagealven"
+#17 "Testeboan"
+
+
+setwd("C:/WGBAST15/WGBAST_2025")
+source("flhm/paths_FLHM.R") #windows
+
+
+modelName<-"FLHM_Nimble_2025_CR_base4"      
+
+CR<-T
+source(paste0(PathFunctions,"plotfunctions.r"))
+source(paste0(PathModel,"make_JAGS_data_",assessment_year,"_base4.R"))
+
+source("flhm/make_inits_Nimble.r")
+
+source(paste0(PathModel,modelName,".R"))
+
+if(parallel==T){
+  parr_code<-paste0("Parallel_code_Nimble_",assessment_year,".R")
+  modelfile<-paste0(PathModel,modelName,".R")
+  source(paste0(PathModel,parr_code))
+}
+
+
+WGBASTData<-list(SmoltWobs=as.matrix(exp(mu_SmoltW[,stock_indices])),cr_ObsW=cr_ObsW,cl_ObsW=cl_ObsW,cdo_ObsW=cdo_ObsW,cdc_ObsW=cdc_ObsW,cc_ObsW=cc_ObsW,cr_ObsRsp=cr_ObsRsp,cr_ObsR=cr_ObsR,cc_ObsR=cc_ObsR,cl_ObsR=cl_ObsR,cdo_ObsR=cdo_ObsR,cdc_ObsR=cdc_ObsR,ncr_ObsTot=cat_r,ncc_ObsTot=cat_c,nco_ObsTot=cat_o,nct_ObsTot=cat_t,log_WpropObs=log(WpropObs),log_RpropObs=log(1-WpropObs),sp_count=as.matrix(sp_count[,stock_indices]),TrapTot=TrapTot,NLuleRec=NLuleRec,WGrilse=as.matrix(WGrilse[,stock_indices]),WMSW=as.matrix(WMSW[,stock_indices]),ladder_count=as.matrix(ladder_count[,stock_indices]),MSWprop=as.matrix(MSWprop[,stock_indices]))
+                           
+
+WGBASTConsts<-list(Tstep=12,AUS=AUS,m=years,stocks=stocks,rstocks=rstocks,AUR=c(2,3),AU=AU[stock_indices],SR_unit=SR_unit[stock_indices],
+                   M_R0=M_K[stock_indices],tau_R0=tau_K[stock_indices],mu_Parr=as.matrix(mu_Parr[,stock_indices]),tau_Parr=as.matrix(tau_Parr[,stock_indices]),
+                   Smolt_Rsp=as.matrix(Smolt_Rsp[,stock_indices]),mu_SmoltW=as.matrix(mu_SmoltW[,stock_indices]),tau_SmoltW=as.matrix(tau_SmoltW[,stock_indices]),
+                   Er=Er,El=El,Edo=Edo,Edc=Edc,Ectn=Ectn,Ecgn=Ecgn,sealMort=sealMort,rel_W=rel_W1,rel_R=rel_R1,rel_Rsp=rel_Rsp1,reportcAdj=reportcAdj,
+                   muTemp=muTemp,tauTemp=tauTemp,mon_Ec=c(6,2,2,2,2,2),mon_Edc=rep(1,times=6),
+                   mon_E=array(c(1,rep(2,times=5),rep(2,times=6),rep(8,times=6),rep(1,times=6),1,rep(2,times=5),rep(1,times=6)),dim=c(6,6)),
+                  ureport_r=unrep$coef_r,ureport_c=unrep$coef_c,ureport_o=unrep$coef_o,PropCR=cat_ratio[,1],PropCW=cat_ratio[,2],mu_sp_alpha=mu_sp_alpha[stock_indices],
+                   mu_sp_beta=mu_sp_beta[stock_indices],CV_sp_alpha=CV_sp_alpha[stock_indices],CV_sp_beta=CV_sp_beta[stock_indices],SmoltRdata=SmoltRdata,sd_wr=sd_wr,N_sp_count=as.matrix(N_sp_count[,stock_indices]),
+                   RProp=RProp,CatchR=CatchR,NLuleRel=NLuleRel,yLule=yLule,Grilse_all=as.matrix(Grilse_all[,stock_indices]),MSW_all=as.matrix(MSW_all[,stock_indices]),alpha_ladder=as.matrix(alpha_ladder[,stock_indices]),
+                   beta_ladder=as.matrix(beta_ladder[,stock_indices]),au1_stocks=au1_stocks,au2_stocks=au2_stocks,au3_stocks=au3_stocks,au4_stocks=au4_stocks,mu_mu_a=c(-2.784,-2.784),tau_mu_a=c(2.388,2.388),
+                   mu_sd_a=c(-0.2653,-0.2653),tau_sd_a=c(3.1529,3.1529),smolt_year=smolt_year,e_delay=e_delay,rivHR=as.matrix(rivHR[,stock_indices]), maxvar = 0.5 / 12,prop_fem=prop_fem,alpha_migr=alpha_migr,beta_migr=beta_migr,M74_mu=as.matrix(M74_mu[,stock_indices]),M74_tau=as.matrix(M74_tau[,stock_indices]),alpha_rel=alpha_rel,beta_rel=beta_rel,mu_mu_sp=mu_mu_sp[stock_indices],tau_mu_sp=tau_mu_sp[stock_indices],
+mu_CV_sp=mu_CV_sp[stock_indices],tau_CV_sp=tau_CV_sp[stock_indices])
+                   
+
+WGBASTInits<-make.inits()
+
+rm(d, chains)
+
+print(paste0(modelName,"_data", assessment_year))
+
+  parnames<-c("coefDS","tau_MpsW","MpsW","MpsR","mu_MpsW","NspWtot","SmoltR",
+            "SmoltW","EPR","EPR_M74","alphaSR","betaSR","tau_SR","z","K","R0",
+            "nco_ObsTotX","ncr_ObsTotX","ncc_ObsTotX",
+            "sp_countX","LW","LR","M74",
+            "Eggstot","Eggstot_M74","MW","MR","fec","NccW","NccR","probMSW",
+            "NladderW_tot", "Usmolt","pTrap","NrRtot","delta","bL","tauL",
+            "LReffect","cL","mucL","taucL","Wprop","tauCR","tauCC","tauCO","tauCT",
+            "p.ladder","p.detect",
+            "HrW","HrR","HdoW","HdoR","HdcW","HdcR","HlW","HlR","HcW","HcR",
+            
+            "HtW", "HtR","phi_tr","mean_trW", "nct_ObsTotX", "sd_tr", # Params of trolling
+            "phi_ql", "mean_qlW","sd_ql", # AR(1) params of ql
+            "phi_qd", "mean_qdW","sd_qd", #AR(1) params of qd
+            "phi_tr", "mean_trW","sd_tr", #AR(1) params of trolling
+            "NrAll_tot","NspWtot","Tretain","muCC",
+            "muCO","muCR","nc_oll_Tot","nc_odn_Tot",
+            "qcgnR","qcgnW","qctnR","qctnW","qdR","qdW","qrR","qrW","qlR","qlW",
+            "reportc","reportd","reportl","reportrR","reportrW","rrR",
+            "surv_migr","logit_qlW","early_MpsW","mu_MpsW","tau_MpsW","mu_qlW","tau_ql","nco_W","nco_R","p.mort","p.rel","nctW_rel","nctW_Tot","CV_ladder","mu_CVSR","tau_CVSR","mu_spawn","CV_spawn")         
+               #"mu_CVSR","tau_CVSR"
+######################################################################################3
+
+
+##parallel
+ if(parallel==T){
+ ptm <- proc.time()
+ this_cluster <- makeCluster(4,outfile="")
+ chain_output <- parLapply(cl = this_cluster, X = 1:2, 
+                           fun = run_wgbastCode, 
+                           wdata = WGBASTData,wconsts=WGBASTConsts,winits=WGBASTInits,wmonitor=parnames,mfile=modelfile)
+ 
+ ## It's good practice to close the cluster when you're done with it.
+ stopCluster(this_cluster)
+ 
+ proc.time() - ptm
+ }else if(parallel==F){
+  
+  #nimbleOptions(enableDerivs = TRUE)
+  wgbastModel<-nimbleModel(code = WGBASTCode, constants = WGBASTConsts,
+  data = WGBASTData, inits=WGBASTInits,calculate=FALSE) # ,buildDerivs = TRUE
+  #wgbastModel$calculate()
+  
+  stnodes <- wgbastModel$getNodeNames(stochOnly = TRUE, includeData = FALSE)
+  allvars<-wgbastModel$getVarNames(nodes = stnodes)
+  mvars<-allvars[!(grepl("lifted",allvars))]  
+  
+  #set.seed(794648) 
+  wgbastModel$simulate(wgbastModel$getDependencies(stnodes))
+  wgbastModel$calculate()
+  
+  for(i in 1:length(mvars)){
+  print(paste0(mvars[i]," ",wgbastModel$calculate(mvars[i]) ))
+  }
+  
+
+
+    
+  wgbastConf <- configureMCMC(wgbastModel, print=TRUE, useConjugacy = FALSE, monitors = parnames) 
+  wMCMC <- buildMCMC(wgbastConf) # uncompiled R code 
+
+  
+  Cwgbast <- compileNimble(wgbastModel,get_mu_tau,showCompilerOutput = TRUE)
+ 
+  CwgbastMCMC <- compileNimble(wMCMC, project = wgbastModel)
+  
+
+  
+ptm<-proc.time()
+CwgbastMCMC$run(1000, time = TRUE, nburnin=500, thin=1)  #2000 took 2800s
+print(proc.time()-ptm)   #2043 without slice sampler MW MR
+print("done") 
+}
+
+save(chain_output,file="Nimble_base4_2025.RData")
+
+v1 <- mcmc(chain_output[[1]]$samples)
+v2 <- mcmc(chain_output[[2]]$samples)
+
+chains<-mcmc.list(list(v1,v2)) 
+
+samples <- as.matrix(CwgbastMCMC$mvSamples)
+chains<-as.mcmc(samples)
+traceplot(chains[,"MW"])
+
