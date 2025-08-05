9d7a5378559df36d10000cd77132af629ac00072
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2025-08-05 08:49:24 +0300

    File to combine extra output from the assessment

---
+++ 05-results/extra-output.R
@@ -0,0 +1,85 @@
+
+
+
+# Extra output that is run after assessment, usually during the summer or early 
+# autumn. Output is stored to shared OneDrive folder.
+# Use no fishing scenario (for most of output only history is of interest)
+
+# Add here later output that is delivered eg. for HELCOM 
+
+# Email from Atso to Henni 13/6/25
+# - Number of salmon arriving to Gulf of Bothnia 
+# o total wild
+# o total reared
+# o total wild + reared
+# o total Simojoki
+# o total Tornionjoki
+# - Number of spawners
+# o total wild per AU
+# o total reared per AU
+# o total Simojoki
+# o total Tornionjoki
+# - Age structure of the spawners, history plus no fishing scenarios until 2050
+# o total wild
+# o total reared
+# o Tornionjoki
+# - Slim, SMSY and SR0 (as number of eggs, or if possible, as number of salmon), 50%, 80% and 95% level of probability
+# o Simojoki
+# o Tornionjoki
+# o all wild swedish stocks for SLU?
+# - Coastal harvest rate per AU, preferably both grilse and MSW (but at least MSW)
+# o wild
+# o reared
+
+
+################################################################################
+# Number of salmon arriving to Gulf of Bothnia
+################################################################################
+# o total wild
+# o total reared
+# o total wild + reared
+# o total Simojoki
+# o total Tornionjoki
+
+################################################################################
+# Number of spawners
+################################################################################
+# o total wild per AU
+# o total reared per AU
+# o total Simojoki
+# o total Tornionjoki
+
+################################################################################
+# - Age structure of the spawners, history plus no fishing scenarios until 2050
+################################################################################
+# o total wild
+# o total reared
+# o Tornionjoki
+
+################################################################################
+# - Slim, SMSY and SR0 (as number of eggs, or if possible, as number of salmon), 50%, 80% and 95% level of probability
+################################################################################
+# o Simojoki
+# o Tornionjoki
+# o all wild swedish stocks for SLU?
+
+################################################################################
+# - Coastal harvest rate per AU, preferably both grilse and MSW (but at least MSW)
+################################################################################
+# o wild
+# o reared
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
