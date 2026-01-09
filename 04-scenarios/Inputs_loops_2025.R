ddf0ddf2112624bd104c5ff96aec9362da9bcd62
Pulkkinen Henni (LUKE) <henni.pulkkinen@luke.fi>
2026-01-09 11:32:42 +0200

    wip

--- 04-scenarios/Inputs_loops_2025.R
+++ 04-scenarios/Inputs_loops_2025.R
@@ -20,7 +20,7 @@ for(loop in 1:(nsim/100)){
   # We will write the simulation results to the sim folder using 10 Rdata-files 
   # under these names 
   BH_dataFile<-
-    paste0(PathScen, "ScenHist_", Model,"_",loop,".RData") # name to separate historical part from future projections?
+    paste0(PathOut_Scen, "ScenHist_", Model,"_",loop,".RData") # name to separate historical part from future projections?
 
   #Sims stores the numbers for the simulation in the MCMC chain
   sims<-c(1+100*(loop-1),100*loop)   #1st and last indices of sims
